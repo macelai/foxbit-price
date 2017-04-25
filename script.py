f96a5e45d14fc08e7acfe9e1dfbe1d13f6d7417f
macelai <vini.macelai@hotmail.com>
2017-04-25 15:09:58 -0300

    Can choose more info

--- script.py
+++ script.py
@@ -1,14 +1,17 @@
 import urllib.request
 from json import loads
 
-def get_info():
+def get_info(type):
     try:
-        request = urllib.request.urlopen("https://api.blinktrade.com/api/v1/BRL/orderbook")
+        request = urllib.request.urlopen("https://api.blinktrade.com/api/v1/BRL/" + type)
 
     except:
         raise RunTimeError("Request failed")
     
     info = loads(request.read().decode("utf-8")) # parses json
-    return info
+    return info    
 
-print(get_info())
+    
+
+
+print(get_info("orderbook"))
