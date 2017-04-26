306a1425d0bea8076c479e1ae31d58b5b98d1f25
macelai <vini.macelai@hotmail.com>
2017-04-26 12:53:40 -0300

    Processing data

---
+++ main.py
@@ -0,0 +1,6 @@
+from sys import argv
+
+import script
+
+if __name__ == "__main__":
+    print("Teste")   
--- script.py
+++ script.py
@@ -1,17 +1,35 @@
 import urllib.request
 from json import loads
 
-def get_info(type):
+def get_data(type):
     try:
         request = urllib.request.urlopen("https://api.blinktrade.com/api/v1/BRL/" + type)
 
     except:
         raise RunTimeError("Request failed")
     
-    info = loads(request.read().decode("utf-8")) # parses json
-    return info    
+    data = loads(request.read().decode("utf-8")) # parses json
+    return data    
 
-    
+def process_orderbook(data, amount):
+    bids = data["bids"]
+    asks = data["asks"]
+    print("Buy orders")
+    for i in range(0, amount):
+        bids[i].pop(-1)
+        print(bids[i])
+
+    print("\n" + "Sell orders")
+    for i in range(0, amount):
+        asks[i].pop(-1)
+        print(asks[i])
+
+def process_general_data(data):
+    print("\nLast buy: " + str(data["last"]))
 
 
-print(get_info("orderbook"))
+info = get_data("ticker")
+orderbook = get_data("orderbook")
+process_orderbook(orderbook, 3)
+process_general_data(info)
+
