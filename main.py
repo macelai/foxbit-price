a46d44ca74dc69787ec7366f11fb3025bcddb79f
macelai <vini.macelai@hotmail.com>
2017-05-17 13:53:26 -0300

    Add default case

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from script import *
 
 if __name__ == "__main__":
     
+    if len(argv) == 1:
+        argv.append(3) #if no argument, default = 3
+
     if argv[1] == "--help":
         print("Type ./main.py and the number of orders\n"
                "Like ./main.py 5, to see 5 orders")
+
     else:       
         info = get_data("ticker")
         orderbook = get_data("orderbook")
